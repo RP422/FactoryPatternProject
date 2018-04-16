2f173ad269bb4f489755019370e3f95ee62d6114
Mike Lang <mlang@student.neumont.edu>
2018-04-16 17:32:24 -0600

    - Needed modification for successful pass-off. Final Commit.

--- GuiBuilderUI/MainWindow.xaml.cs
+++ GuiBuilderUI/MainWindow.xaml.cs
@@ -22,11 +22,7 @@ namespace GuiBuilderUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private LanguageFactory[] allFactories = // To add a new language to this window, the only change you would 
-        {                                        //   need to make is adding a reference to an instance of it here.
-            new WpfLanguageFactory(),
-            new HtmlLanguageFactory()
-        };
+        private LanguageFactory[] allFactories = LanguageFactoryFactory.CreateAvailableLanguages();
 
         private LanguageFactory currentFactory;
 
---
+++ Library/LanguageFactoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LanguageFactoryFactory
+    {
+        public static LanguageFactory[] CreateAvailableLanguages()
+        {
+            return new LanguageFactory[] // To add a new language to this window, the only change you would 
+            {                            //   need to make is adding a reference to an instance of it here.
+                new WpfLanguageFactory(),
+                new HtmlLanguageFactory()
+            };
+        }
+    }
+}
